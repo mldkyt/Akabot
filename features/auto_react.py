a9a4f24282ea35a4abe3d39525ebd34d9b3d5b2c
mldchan <mldkyt@proton.me>
2024-10-28 23:47:26 +0100

    Try catch statements everywhere

--- features/auto_react.py
+++ features/auto_react.py
@@ -1,5 +1,6 @@
 import discord
 import emoji as emoji_package
+import sentry_sdk
 from bson import ObjectId
 from discord.ext import commands as discord_commands_ext
 from discord.ext import pages as dc_pages
@@ -22,156 +23,182 @@ class AutoReact(discord.Cog):
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def add_auto_react(self, ctx: discord.ApplicationContext, trigger: str, emoji_input: str):
-        # Parse le emoji
-        emoji = discord.PartialEmoji.from_str(emoji_input.strip())
+        try:
+            # Parse le emoji
+            emoji = discord.PartialEmoji.from_str(emoji_input.strip())
 
-        if emoji.is_custom_emoji():
-            # Custom emoji check
-            if emoji.id is None:
-                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
-                return
+            if emoji.is_custom_emoji():
+                # Custom emoji check
+                if emoji.id is None:
+                    await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
+                    return
 
-            emoji_s = await ctx.guild.fetch_emoji(emoji.id)
-            if emoji_s is None:
-                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
-                return
-        else:
-            # Regex check
-            if not emoji_package.is_emoji(emoji_input):
-                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
-                return
+                emoji_s = await ctx.guild.fetch_emoji(emoji.id)
+                if emoji_s is None:
+                    await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
+                    return
+            else:
+                # Regex check
+                if not emoji_package.is_emoji(emoji_input):
+                    await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
+                    return
 
-        client['AutoReact'].insert_one({'GuildID': str(ctx.guild.id), 'TriggerText': trigger, 'Emoji': str(emoji)})
+            client['AutoReact'].insert_one({'GuildID': str(ctx.guild.id), 'TriggerText': trigger, 'Emoji': str(emoji)})
 
-        # Respond
-        await ctx.respond(
-            trl(ctx.user.id, ctx.guild.id, "auto_react_add_success").format(trigger=trigger, emoji=str(emoji)),
-            ephemeral=True)
+            # Respond
+            await ctx.respond(
+                trl(ctx.user.id, ctx.guild.id, "auto_react_add_success").format(trigger=trigger, emoji=str(emoji)),
+                ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @auto_react_group.command(name="list", description="List auto react settings")
     @discord_commands_ext.has_permissions(manage_messages=True)
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def list_auto_reacts(self, ctx: discord.ApplicationContext):
-        # Define a message variable
-        message_groups = []
-        current_group = 0
-        current_group_msg = ""
+        try:
+            # Define a message variable
+            message_groups = []
+            current_group = 0
+            current_group_msg = ""
 
-        # List
-        all_settings = client['AutoReact'].find({'GuildID': {'$eq': str(ctx.guild.id)}}).to_list()
-        if len(all_settings) == 0:
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_list_empty"), ephemeral=True)
-            return
+            # List
+            all_settings = client['AutoReact'].find({'GuildID': {'$eq': str(ctx.guild.id)}}).to_list()
+            if len(all_settings) == 0:
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_list_empty"), ephemeral=True)
+                return
 
-        for i in all_settings:
-            # Format: **{id}**: {trigger} -> {reply}
+            for i in all_settings:
+                # Format: **{id}**: {trigger} -> {reply}
 
-            # current_group_msg += f"**ID: `{i[0]}`**: Trigger: `{i[2]}` -> Will react with `{i[3]}`\n"
-            current_group_msg += trl(ctx.user.id, ctx.guild.id, "auto_react_list_row").format(id=str(i['_id']), trigger=i['TriggerText'],
-                                                                                              emoji=i['Emoji'])
-            current_group += 1
+                # current_group_msg += f"**ID: `{i[0]}`**: Trigger: `{i[2]}` -> Will react with `{i[3]}`\n"
+                current_group_msg += trl(ctx.user.id, ctx.guild.id, "auto_react_list_row").format(id=str(i['_id']),
+                                                                                                  trigger=i[
+                                                                                                      'TriggerText'],
+                                                                                                  emoji=i['Emoji'])
+                current_group += 1
 
-            # Split message group
-            if current_group == 5:
-                message_groups.append(current_group)
-                current_group = 0
-                current_group_msg = ""
+                # Split message group
+                if current_group == 5:
+                    message_groups.append(current_group)
+                    current_group = 0
+                    current_group_msg = ""
 
-        # Add last group
-        if current_group != 0:
-            message_groups.append(current_group_msg)
+            # Add last group
+            if current_group != 0:
+                message_groups.append(current_group_msg)
 
-        # Reply with a paginator
-        paginator = dc_pages.Paginator(message_groups)
-        await paginator.respond(ctx.interaction, ephemeral=True)
+            # Reply with a paginator
+            paginator = dc_pages.Paginator(message_groups)
+            await paginator.respond(ctx.interaction, ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @auto_react_group.command(name="edit_trigger", description="Change trigger for a setting")
     @discord_commands_ext.has_permissions(manage_messages=True)
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def edit_trigger_auto_react(self, ctx: discord.ApplicationContext, id: str, new_trigger: str):
-        if not ObjectId.is_valid(id):
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
-                              ephemeral=True)
-            return
+        try:
+            if not ObjectId.is_valid(id):
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
+                                  ephemeral=True)
+                return
 
-        edited = client['AutoReact'].update_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)}, {'$set': {'TriggerText': new_trigger}})
+            edited = client['AutoReact'].update_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)},
+                                                    {'$set': {'TriggerText': new_trigger}})
 
-        if edited.matched_count == 0:
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
-                              ephemeral=True)
-            return
+            if edited.matched_count == 0:
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
+                                  ephemeral=True)
+                return
 
-        await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_keyword_updated_success"), ephemeral=True)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_keyword_updated_success"), ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @auto_react_group.command(name="delete", description="Delete an auto react setting")
     @discord_commands_ext.has_permissions(manage_messages=True)
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def delete_auto_react(self, ctx: discord.ApplicationContext, id: int):
-        delete = client['AutoReact'].delete_one({'ID': id, 'GuildID': str(ctx.guild.id)})
+        try:
+            delete = client['AutoReact'].delete_one({'ID': id, 'GuildID': str(ctx.guild.id)})
 
-        if delete.deleted_count == 0:
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
-                              ephemeral=True)
-            return
+            if delete.deleted_count == 0:
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
+                                  ephemeral=True)
+                return
 
-        # Respond
-        await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_delete_success"), ephemeral=True)
+            # Respond
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_delete_success"), ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @auto_react_group.command(name="edit_react_emoji", description="Change react for a setting")
     @discord_commands_ext.has_permissions(manage_messages=True)
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def edit_response_auto_react(self, ctx: discord.ApplicationContext, id: str, new_emoji: str):
-        # Parse le emoji
-        emoji = discord.PartialEmoji.from_str(new_emoji.strip())
-        if emoji.is_custom_emoji():
-            # Check if the emoji is from the current server
-            if emoji.id is None:
-                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
-                return
+        try:
+            # Parse le emoji
+            emoji = discord.PartialEmoji.from_str(new_emoji.strip())
+            if emoji.is_custom_emoji():
+                # Check if the emoji is from the current server
+                if emoji.id is None:
+                    await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
+                    return
 
-            for i in ctx.guild.emojis:
-                if i.id == emoji.id:
-                    break
+                for i in ctx.guild.emojis:
+                    if i.id == emoji.id:
+                        break
+                else:
+                    await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
+                    return
             else:
-                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
+                if not emoji_package.is_emoji(new_emoji.strip()):
+                    await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
+                    return
+
+            if not ObjectId.is_valid(id):
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
+                                  ephemeral=True)
                 return
-        else:
-            if not emoji_package.is_emoji(new_emoji.strip()):
-                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_invalid"), ephemeral=True)
+
+            updated = client['AutoReact'].update_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)},
+                                                     {'$set': {'Emoji': str(emoji)}})
+
+            if updated.matched_count == 0:
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
+                                  ephemeral=True)
                 return
 
-        if not ObjectId.is_valid(id):
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
-                              ephemeral=True)
-            return
-
-        updated = client['AutoReact'].update_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)},
-                                                 {'$set': {'Emoji': str(emoji)}})
-
-        if updated.matched_count == 0:
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_setting_not_found"),
-                ephemeral=True)
-            return
-
-        # Respond
-        await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_set_success"), ephemeral=True)
+            # Respond
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_react_emoji_set_success"), ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @discord.Cog.listener()
     async def on_message(self, msg: discord.Message):
-        # Check if it's the bot
-        if msg.author.bot:
-            return
+        try:
+            # Check if it's the bot
+            if msg.author.bot:
+                return
 
-        # Find all created settings
-        data = client['AutoReact'].find({'GuildID': str(msg.guild.id)}).to_list()
-        for i in data:
-            # If it contains the message
-            if i['TriggerText'] in msg.content:
-                # Parse the emoji
-                emoji = discord.PartialEmoji.from_str(i['Emoji'])
-                # Add le reaction to it
-                await msg.add_reaction(emoji)
+            # Find all created settings
+            data = client['AutoReact'].find({'GuildID': str(msg.guild.id)}).to_list()
+            for i in data:
+                # If it contains the message
+                if i['TriggerText'] in msg.content:
+                    # Parse the emoji
+                    emoji = discord.PartialEmoji.from_str(i['Emoji'])
+                    # Add le reaction to it
+                    await msg.add_reaction(emoji)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
--- features/auto_response.py
+++ features/auto_response.py
@@ -1,4 +1,5 @@
 import discord
+import sentry_sdk
 from bson import ObjectId
 from discord.ext import commands as discord_commands_ext
 from discord.ext import pages as dc_pages
@@ -21,123 +22,150 @@ class AutoResponse(discord.Cog):
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def add_auto_response(self, ctx: discord.ApplicationContext, trigger: str, reply: str):
-        client['AutoResponse'].insert_one({'GuildID': str(ctx.guild.id), 'TriggerText': trigger, 'ReplyText': reply})
+        try:
+            client['AutoResponse'].insert_one(
+                {'GuildID': str(ctx.guild.id), 'TriggerText': trigger, 'ReplyText': reply})
 
-        # Respond
-        await ctx.respond(
-            trl(ctx.user.id, ctx.guild.id, "auto_response_add_success").format(trigger=trigger, reply=reply),
-            ephemeral=True)
+            # Respond
+            await ctx.respond(
+                trl(ctx.user.id, ctx.guild.id, "auto_response_add_success").format(trigger=trigger, reply=reply),
+                ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @auto_response_group.command(name="list", description="List auto response settings")
     @discord_commands_ext.has_permissions(manage_messages=True)
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def list_auto_responses(self, ctx: discord.ApplicationContext):
-        # Define a message variable
-        message_groups = []
-        current_group = 0
-        current_group_msg = ""
+        try:
+            # Define a message variable
+            message_groups = []
+            current_group = 0
+            current_group_msg = ""
 
-        # List
+            # List
 
-        all_settings = client['AutoResponse'].find({'GuildID': str(ctx.guild.id)}).to_list()
-        if len(all_settings) == 0:
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_list_empty"), ephemeral=True)
-            return
+            all_settings = client['AutoResponse'].find({'GuildID': str(ctx.guild.id)}).to_list()
+            if len(all_settings) == 0:
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_list_empty"), ephemeral=True)
+                return
 
-        for i in all_settings:
-            # Format: **{id}**: {trigger} -> {reply}
-            # current_group_msg += f"**ID: `{i[0]}`**: Trigger: `{i[2]}` -> Will say `{i[3]}`\n"
-            current_group_msg += trl(ctx.user.id, ctx.guild.id, "auto_response_list_row").format(id=str(i['_id']), trigger=i['TriggerText'],
-                                                                                                 reply=i['ReplyText'])
-            current_group += 1
+            for i in all_settings:
+                # Format: **{id}**: {trigger} -> {reply}
+                # current_group_msg += f"**ID: `{i[0]}`**: Trigger: `{i[2]}` -> Will say `{i[3]}`\n"
+                current_group_msg += trl(ctx.user.id, ctx.guild.id, "auto_response_list_row").format(id=str(i['_id']),
+                                                                                                     trigger=i[
+                                                                                                         'TriggerText'],
+                                                                                                     reply=i[
+                                                                                                         'ReplyText'])
+                current_group += 1
 
-            # Split message group
-            if current_group == 5:
-                message_groups.append(current_group)
-                current_group = 0
-                current_group_msg = ""
+                # Split message group
+                if current_group == 5:
+                    message_groups.append(current_group)
+                    current_group = 0
+                    current_group_msg = ""
 
-        # Add last group
-        if current_group != 0:
-            message_groups.append(current_group_msg)
+            # Add last group
+            if current_group != 0:
+                message_groups.append(current_group_msg)
 
-        # Reply with a paginator
-        paginator = dc_pages.Paginator(message_groups)
-        await paginator.respond(ctx.interaction, ephemeral=True)
+            # Reply with a paginator
+            paginator = dc_pages.Paginator(message_groups)
+            await paginator.respond(ctx.interaction, ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @auto_response_group.command(name="edit_trigger", description="Change trigger for a setting")
     @discord_commands_ext.has_permissions(manage_messages=True)
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def edit_trigger_auto_response(self, ctx: discord.ApplicationContext, id: str, new_trigger: str):
-        if not ObjectId.is_valid(id):
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
-                              ephemeral=True)
-            return
+        try:
+            if not ObjectId.is_valid(id):
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
+                                  ephemeral=True)
+                return
 
-        result = client['AutoResponse'].update_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)},
-                                                {'$set': {'TriggerText': new_trigger}})
+            result = client['AutoResponse'].update_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)},
+                                                       {'$set': {'TriggerText': new_trigger}})
 
-        if result.matched_count == 0:
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
-                              ephemeral=True)
-            return
+            if result.matched_count == 0:
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
+                                  ephemeral=True)
+                return
 
-        # Respond
-        await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_keyword_updated"), ephemeral=True)
+            # Respond
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_keyword_updated"), ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @auto_response_group.command(name="delete", description="Delete an auto response setting")
     @discord_commands_ext.has_permissions(manage_messages=True)
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def delete_auto_response(self, ctx: discord.ApplicationContext, id: str):
-        if not ObjectId.is_valid(id):
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
-                              ephemeral=True)
-            return
+        try:
+            if not ObjectId.is_valid(id):
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
+                                  ephemeral=True)
+                return
 
-        result = client['AutoResponse'].delete_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)})
+            result = client['AutoResponse'].delete_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)})
 
-        if result.deleted_count == 0:
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
-                              ephemeral=True)
-            return
+            if result.deleted_count == 0:
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
+                                  ephemeral=True)
+                return
 
-        # Respond
-        await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_delete_success"), ephemeral=True)
+            # Respond
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_delete_success"), ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @auto_response_group.command(name="edit_response", description="Change response for a setting")
     @discord_commands_ext.has_permissions(manage_messages=True)
     @discord_commands_ext.bot_has_permissions(read_message_history=True, add_reactions=True)
     @discord.default_permissions(manage_messages=True)
     async def edit_response_auto_response(self, ctx: discord.ApplicationContext, id: str, new_response: str):
-        if not ObjectId.is_valid(id):
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
-                              ephemeral=True)
-            return
+        try:
+            if not ObjectId.is_valid(id):
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
+                                  ephemeral=True)
+                return
 
-        result = client['AutoResponse'].update_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)},
-                                                {'$set': {'ReplyText': new_response}})
+            result = client['AutoResponse'].update_one({'_id': ObjectId(id), 'GuildID': str(ctx.guild.id)},
+                                                       {'$set': {'ReplyText': new_response}})
 
-        if result.matched_count == 0:
-            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
-                              ephemeral=True)
-            return
+            if result.matched_count == 0:
+                await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_setting_not_found"),
+                                  ephemeral=True)
+                return
 
-        # Respond
-        await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_text_updated"), ephemeral=True)
+            # Respond
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "auto_response_text_updated"), ephemeral=True)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
 
     @discord.Cog.listener()
     async def on_message(self, msg: discord.Message):
-        # Check if it's the bot
-        if msg.author.bot:
-            return
+        try:
+            # Check if it's the bot
+            if msg.author.bot:
+                return
 
-        # Find all created settings
-        data = client['AutoResponse'].find({'GuildID': str(msg.guild.id)})
-        for i in data:
-            # If it contains the message
-            if i['TriggerText'] in msg.content:
-                # Reply to it
-                await msg.reply(i['ReplyText'])
+            # Find all created settings
+            data = client['AutoResponse'].find({'GuildID': str(msg.guild.id)})
+            for i in data:
+                # If it contains the message
+                if i['TriggerText'] in msg.content:
+                    # Reply to it
+                    await msg.reply(i['ReplyText'])
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
