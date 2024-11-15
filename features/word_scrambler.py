ba7a7bbbc97f6fb90e162f44d3d7fc0174344a02
mldchan <mldkyt@proton.me>
2024-11-15 22:13:37 +0100

    chore: per channel settings for Word Scrambler

--- features/word_scrambler.py
+++ features/word_scrambler.py
@@ -1,11 +1,14 @@
 import datetime
+import random
 import re
 
 import discord
 import sentry_sdk
 
 from database import client
-from utils.english_words import get_random_english_word, scramble_word
+from utils.english_words import get_random_english_word, scramble_word, get_random_english_word_range
+from utils.settings import get_setting, set_setting
+from discord.ext import commands, tasks
 
 
 def start_word_scrambler(channel_id: int, message_id: int, word: str) -> str:
@@ -69,10 +72,36 @@ class WordScrambler(discord.Cog):
         super().__init__()
         self.bot = bot
 
-    @discord.slash_command(name="debug_word_scramble", guild_ids=[1234573274504237087])
-    async def debug_word_scramble(self, ctx: discord.ApplicationContext):
+    # @discord.slash_command(name="debug_word_scramble", guild_ids=[1234573274504237087])
+    # async def debug_word_scramble(self, ctx: discord.ApplicationContext):
+    #
+    #     try:
+    #         word = get_random_english_word()
+    #         scrambled_word = scramble_word(word)
+    #
+    #         msg = await ctx.channel.send("# Word Scramble!\nUnscramble the word below:\n\n`" + scrambled_word + "`")
+    #         start_word_scrambler(ctx.channel.id, msg.id, word)
+    #
+    #         await ctx.respond("Word scrambler started", ephemeral=True)
+    #     except Exception as e:
+    #         await ctx.respond("Failed to start word scrambler", ephemeral=True)
+    #         sentry_sdk.capture_exception(e)
 
+    @discord.Cog.listener()
+    async def on_ready(self):
+        self.random_word_scramble_task.start()
+
+    word_scramble_commands = discord.SlashCommandGroup(name="word_scramble", description="Word Scramble commands")
+
+    @word_scramble_commands.command(name="start", description="Force start a word scramble game in this channel")
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    async def word_scramble_start(self, ctx: discord.ApplicationContext):
         try:
+            if is_word_scrambler_running(ctx.channel.id):
+                await ctx.respond("A word scramble game is already running in this channel", ephemeral=True)
+                return
+
             word = get_random_english_word()
             scrambled_word = scramble_word(word)
 
@@ -82,6 +111,82 @@ class WordScrambler(discord.Cog):
             await ctx.respond("Word scrambler started", ephemeral=True)
         except Exception as e:
             await ctx.respond("Failed to start word scrambler", ephemeral=True)
+            sentry_sdk.capture_exception(e)
+
+    @word_scramble_commands.command(name="end", description="Force end a word scramble game in this channel")
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    async def word_scramble_end(self, ctx: discord.ApplicationContext):
+        try:
+            if not is_word_scrambler_running(ctx.channel.id):
+                await ctx.respond("No word scramble game is running in this channel", ephemeral=True)
+                return
+
+            message = get_word_scrambler_original_message(ctx.channel.id)
+            if message is not None:
+                message = await ctx.channel.fetch_message(message)
+                await message.edit(content="Word scramble game ended forcibly by " + ctx.author.mention)
+
+            end_word_scrambler(ctx.channel.id)
+            await ctx.respond("Word scrambler ended", ephemeral=True)
+        except Exception as e:
+            await ctx.respond("Failed to end word scrambler", ephemeral=True)
+            sentry_sdk.capture_exception(e)
+
+    @word_scramble_commands.command(name="set_channel", description="Add a channel or set settings for word scramble")
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    @discord.option(name="chance", description="One in what chance to start per minute? Higher value = Less occurence", type=int, required=True)
+    @discord.option(name="min", description="Minimum word length", type=int, required=True)
+    @discord.option(name="max", description="Maximum word length", type=int, required=True)
+    async def word_scramble_set_channel(self, ctx: discord.ApplicationContext, channel: discord.TextChannel, chance: int, min_characters: int, max_characters: int):
+        try:
+            if min_characters < 3:
+                await ctx.respond("Minimum word length must be at least 3", ephemeral=True)
+                return
+
+            if max_characters > 20:
+                await ctx.respond("Maximum word length must be at most 20", ephemeral=True)
+                return
+
+            if min_characters > max_characters:
+                await ctx.respond("Minimum word length must be less than or equal to maximum word length", ephemeral=True)
+                return
+
+            if chance < 1:
+                await ctx.respond("Chance must be at least 1", ephemeral=True)
+                return
+
+            sett = get_setting(ctx.guild.id, "word_scramble_channels", {})
+            sett[str(channel.id)] = {
+                "chance": chance,
+                "min": min_characters,
+                "max": max_characters
+            }
+
+            set_setting(ctx.guild.id, "word_scramble_channels", sett)
+            await ctx.respond(f"Channel set\n"
+                              f"**Chance**: 1 in {chance} chance\n"
+                              f"**Minimum Letters**: {min_characters}\n"
+                              f"**Maximum Letters**: {max_characters}\n", ephemeral=True)
+        except Exception as e:
+            await ctx.respond("Failed to set channel", ephemeral=True)
+            sentry_sdk.capture_exception(e)
+
+    @word_scramble_commands.command(name="remove_channel", description="Remove a channel from word scramble")
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    async def word_scramble_remove_channel(self, ctx: discord.ApplicationContext, channel: discord.TextChannel):
+        try:
+            sett = get_setting(ctx.guild.id, "word_scramble_channels", {})
+            if channel.id in sett:
+                del sett[str(channel.id)]
+                set_setting(ctx.guild.id, "word_scramble_channels", sett)
+                await ctx.respond("Channel removed", ephemeral=True)
+            else:
+                await ctx.respond("Channel not found", ephemeral=True)
+        except Exception as e:
+            await ctx.respond("Failed to remove channel", ephemeral=True)
             sentry_sdk.capture_exception(e)
 
     @discord.Cog.listener()
@@ -105,8 +210,52 @@ class WordScrambler(discord.Cog):
 
                 await orig.edit(
                     content="# Word Scramble!\n\nWon by " + msg.author.mention + "\nThe word was: `" + msg.content + "`")
-                await msg.reply("Correct! The word was: `" + msg.content + "`")
+                rep = await msg.reply("Correct! The word was: `" + msg.content + "`")
+                await rep.delete(delay=5)
 
                 end_word_scrambler(msg.channel.id)
         except Exception as e:
             sentry_sdk.capture_exception(e)
+
+    @tasks.loop(minutes=1)
+    async def random_word_scramble_task(self):
+        await self.random_word_scramble()
+
+    async def random_word_scramble(self):
+        for guild in self.bot.guilds:
+            channels = get_setting(guild.id, "word_scramble_channels", {})
+            for channel_id, settings in channels.items():
+                channel_id = int(channel_id)
+                if is_word_scrambler_running(channel_id):
+                    continue
+
+                rand = random.randint(1, settings["chance"])
+                if rand == 1:
+                    word = get_random_english_word_range(settings["min"], settings["max"])
+                    if len(word) < settings["min"] or len(word) > settings["max"]:
+                        continue
+
+                    scrambled_word = scramble_word(word)
+                    channel = self.bot.get_channel(channel_id)
+                    if channel is None:
+                        continue
+
+                    msg = await channel.send("# Word Scramble!\nUnscramble the word below:\n\n`" + scrambled_word + "`")
+                    start_word_scrambler(channel_id, msg.id, word)
+
+    @tasks.loop(minutes=1)
+    async def cleanup_task(self):
+        await self.cleanup()
+
+    async def cleanup(self):
+        expired = client["WordScramble"].find({"Expires": {"$lt": datetime.datetime.now()}})
+        for game in expired:
+            channel = self.bot.get_channel(int(game["ChannelID"]))
+            if channel is None:
+                continue
+
+            message = await channel.fetch_message(int(game["MessageID"]))
+            await message.edit(content=f"# Word Scramble!\n\n"
+                                       f"Game over! The word was: `{game['Word']}`! Better luck next time!")
+            end_word_scrambler(int(game["ChannelID"]))
+
--- utils/english_words.py
+++ utils/english_words.py
@@ -18,6 +18,20 @@ def get_random_english_word() -> str:
 
     return random.choice(text_file)
 
+def get_random_english_word_range(min_length: int, max_length: int) -> str | None:
+    with open("data/words.txt") as f:
+        text_file = f.read().splitlines()
+
+    i = 0
+    while True:
+        i += 1
+        word = random.choice(text_file)
+        if min_length <= len(word) <= max_length:
+            return word
+
+        if i > 10000:
+            return None
+
 def scramble_word(word):
     # If word is too short to scwambwe, wetuwn it as it is >w<
     if len(word) <= 3:
