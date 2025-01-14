93b1df8e3ac5edc1a696f63550f129113475f5ad
mldchan <mldkyt@proton.me>
2025-01-14 09:34:43 +0100

    Chore: V2 Suggestions can now be edited after being posted

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,20 @@
 FROM python:3.12-alpine
 WORKDIR /app
-COPY requirements.txt /app/
 
-RUN pip install --no-cache-dir -r /app/requirements.txt
+COPY requirements.txt .
 
-COPY features /app/features
-COPY utils /app/utils
-COPY database.py /app
-COPY main.py /app/
-COPY LATEST_3.4.md /app/
-COPY LATEST_3.2.md /app/
-COPY LATEST_3.1.md /app/
-COPY lang/ /app/lang/
-COPY docs/ /app/docs/
-COPY configs/ /app/configs/
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY features features/.
+COPY utils utils/.
+COPY database.py .
+COPY main.py .
+COPY LATEST_3.4.md .
+COPY LATEST_3.2.md .
+COPY LATEST_3.1.md .
+COPY lang/ lang/.
+COPY docs/ docs/.
+COPY configs/ configs/.
 
 STOPSIGNAL SIGKILL
 
--- features/suggestions.py
+++ features/suggestions.py
@@ -14,16 +14,18 @@
 #      You should have received a copy of the GNU Affero General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-
+import asyncio
+import logging
 from typing import Any
 
 import discord
 import sentry_sdk
-from discord import Color
+from discord import Color, Interaction
 from discord.ext import commands
-from discord.ui import View
+from discord.ui import View, Modal, InputText
 
 from database import client
+from utils.analytics import analytics
 from utils.languages import get_translation_for_key_localized as trl
 from utils.settings import get_setting, set_setting
 
@@ -43,7 +45,15 @@ def migration_1():
                                                             'Downvotes': [str(x) for x in downvotes]}})
 
 
-async def perform_basic_checks(interaction: discord.Interaction) -> bool | tuple[bool, Any]:
+async def perform_basic_vote_checks(interaction: discord.Interaction) -> bool | tuple[bool, Any]:
+    """
+    Perform basic checks for voting on a suggestion. Checks for DB record and if the user has already voted.
+    Args:
+        interaction: Interaction
+
+    Returns: False or True and the data when succeeded
+
+    """
     data = client['SuggestionMessagesV2'].find_one({'MessageID': str(interaction.message.id)})
     if not data:
         await interaction.response.send_message('This suggestion does not exist in the database', ephemeral=True)
@@ -56,7 +66,15 @@ async def perform_basic_checks(interaction: discord.Interaction) -> bool | tuple
     return True, data
 
 
-async def update_existing_suggestions_message(interaction: discord.Interaction):
+async def update_existing_suggestions_message_interaction(interaction: discord.Interaction):
+    """
+    Update the existing suggestions message with the new vote count
+    Args:
+        interaction: Interaction
+
+    Returns: Nothing
+
+    """
     suggestions_msg = client['SuggestionMessagesV2'].find_one({'MessageID': str(interaction.message.id)})
 
     author = interaction.guild.get_member(int(suggestions_msg['AuthorID']))
@@ -68,31 +86,56 @@ async def update_existing_suggestions_message(interaction: discord.Interaction):
     await interaction.response.edit_message(embed=new_emb)
 
 
+async def update_existing_suggestions_message(msg: discord.Message):
+    """
+    Update the existing suggestions message with the new vote count
+    Args:
+        msg: Message object to update
+
+    Returns: Nothing
+    """
+    suggestions_msg = client['SuggestionMessagesV2'].find_one({'MessageID': str(msg.id)})
+
+    if not suggestions_msg:
+        return
+
+    logging.info('Refreshing message %s', msg.id)
+
+    author = msg.guild.get_member(int(suggestions_msg['AuthorID']))
+
+    new_emb = discord.Embed(title='Suggestion', color=Color.blue(),
+                            author=discord.EmbedAuthor(name=f'{author.display_name}\'s suggestion',
+                                                       icon_url=author.display_avatar.url),
+                            description=generate_message_content(str(msg.id)))
+
+    await msg.edit(embed=new_emb)
+
+
 class V2SuggestionView(View):
     def __init__(self):
         super().__init__(timeout=None)
 
     @discord.ui.button(label='👍', style=discord.ButtonStyle.primary, custom_id='upvote')
     async def upvote(self, button: discord.ui.Button, interaction: discord.Interaction):
-        success, data = await perform_basic_checks(interaction)
+        success, data = await perform_basic_vote_checks(interaction)
         if not success:
             return
 
         client['SuggestionMessagesV2'].update_one({'MessageID': str(interaction.message.id)},
                                                   {'$push': {'Upvotes': str(interaction.user.id)}})
 
-        await update_existing_suggestions_message(interaction)
+        await update_existing_suggestions_message_interaction(interaction)
 
     @discord.ui.button(label='👎', style=discord.ButtonStyle.primary, custom_id='downvote')
     async def downvote(self, button: discord.ui.Button, interaction: discord.Interaction):
-        success, data = await perform_basic_checks(interaction)
+        success, data = await perform_basic_vote_checks(interaction)
         if not success:
             return
 
         client['SuggestionMessagesV2'].update_one({'MessageID': str(interaction.message.id)},
                                                   {'$push': {'Downvotes': str(interaction.user.id)}})
 
-        await update_existing_suggestions_message(interaction)
+        await update_existing_suggestions_message_interaction(interaction)
 
     @discord.ui.button(label='See more information', style=discord.ButtonStyle.primary, custom_id='more_info')
     async def get_info(self, button: discord.ui.Button, interaction: discord.Interaction):
@@ -104,7 +147,7 @@ class V2SuggestionView(View):
         upvotes = data['Upvotes']
         downvotes = data['Downvotes']
         message = data['Suggestion']
-        author = interaction.guild.get_member(int(data['AuthorID'])).mention
+        author = interaction.guild.get_member(int(data['AuthorID']))
         percent = len(upvotes) / (len(upvotes) + len(downvotes)) * 100 if len(upvotes) + len(downvotes) > 0 else 0
 
         upvoters = [f'<@{x}>' for x in upvotes]
@@ -114,17 +157,46 @@ class V2SuggestionView(View):
         if len(downvoters) == 0:
             downvoters = ['No downvoters']
 
-        await interaction.response.send_message(f"""## Information about the suggestion:
-{message}
+        await interaction.response.send_message(
+            trl(interaction.user.id, interaction.guild.id, 'suggestions_v2_information', append_tip=True)
+            .format(message=message, author=author.mention, upvotes=str(len(upvotes)), downvotes=str(len(downvotes)),
+                    percent=f"{percent:.0f}",
+                    upvoters=', '.join(upvoters), downvoters=', '.join(downvoters)),
+            ephemeral=True, view=V2SuggestionAuthorExtraView(msg_id=data['MessageID'],
+                                                             ch_id=str(interaction.channel.id)) if str(
+                interaction.user.id) == data['AuthorID'] else None)
 
-**Author**: {author}
-**Upvotes**: {len(upvotes)}
-**Downvotes**: {len(downvotes)}
-**Upvote rate**: {percent:.0f}%
 
-**Upvoters**: {', '.join(upvoters)}
-**Downvoters**: {', '.join(downvoters)}
-""", ephemeral=True)
+class V2SuggestionEditMessageModal(Modal):
+    def __init__(self, msg_id: str, ch_id: str):
+        super().__init__(timeout=300, title='Edit Message')
+
+        self.msg_id = msg_id
+        self.ch_id = ch_id
+
+        self.add_item(InputText(label='New Message', placeholder='Enter the new message here', max_length=2000))
+
+    async def callback(self, interaction: Interaction):
+        client['SuggestionMessagesV2'].update_one({'MessageID': self.msg_id},
+                                                  {'$set': {'Suggestion': self.children[0].value}})
+
+        ch = interaction.guild.get_channel(int(self.ch_id))
+        msg = await ch.fetch_message(int(self.msg_id))
+
+        await update_existing_suggestions_message(msg)
+        await interaction.respond('Message updated', ephemeral=True)
+
+
+class V2SuggestionAuthorExtraView(View):
+    def __init__(self, msg_id: str, ch_id: str):
+        super().__init__(timeout=120)
+        self.msg_id = msg_id
+        self.ch_id = ch_id
+
+    @discord.ui.button(label='Update Message')
+    async def update_message(self, button: discord.ui.Button, interaction: discord.Interaction):
+        modal = V2SuggestionEditMessageModal(msg_id=self.msg_id, ch_id=self.ch_id)
+        await interaction.response.send_modal(modal)
 
 
 def generate_message_content(id: str):
@@ -266,3 +338,18 @@ class Suggestions(discord.Cog):
         except Exception as e:
             sentry_sdk.capture_exception(e)
             await ctx.respond(trl(ctx.user.id, ctx.guild.id, "command_error_generic"), ephemeral=True)
+
+    @suggestions_group.command(name='update_message', description='Refresh all suggestions messages in this channel')
+    @commands.guild_only()
+    @analytics('suggestions update_message')
+    @commands.cooldown(1, 300, commands.BucketType.guild)  # Cooldown to prevent abuse
+    async def cmd_update_message(self, ctx: discord.ApplicationContext):
+        await ctx.defer(ephemeral=True)
+
+        tasks = []
+        async for msg in ctx.history(limit=1000):
+            tasks.append(update_existing_suggestions_message(msg))
+
+        await asyncio.gather(*tasks)
+
+        await ctx.respond(trl(ctx.user.id, ctx.guild.id, 'suggestions_v2_refreshed', append_tip=True))
